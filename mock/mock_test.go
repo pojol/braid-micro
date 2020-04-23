5fa3eacccb8a9e88697060325c9eeed387a32179
pojol <pojol@outlook.com>
2020-04-23 14:07:10 +0800

    add mock test

--- mock/mock_test.go
+++ mock/mock_test.go
@@ -1,6 +1,7 @@
 package mock
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,10 +9,27 @@ import (
 
 func TestMock(t *testing.T) {
 
+	tredis := os.Getenv(mockRedisEnv)
+	tconsul := os.Getenv(mockConsulEnv)
+	tjaeger := os.Getenv(mockJaegerEnv)
+
+	os.Setenv(mockRedisEnv, "")
+	os.Setenv(mockConsulEnv, "")
+	os.Setenv(mockJaegerEnv, "")
+
 	Init()
 
 	assert.NotEqual(t, mockRedisEnv, "")
 	assert.NotEqual(t, mockConsulEnv, "")
 	assert.NotEqual(t, mockJaegerEnv, "")
 
+	os.Setenv(mockRedisEnv, tredis)
+	os.Setenv(mockConsulEnv, tconsul)
+	os.Setenv(mockJaegerEnv, tjaeger)
+
+	Init()
+
+	assert.NotEqual(t, mockRedisEnv, "")
+	assert.NotEqual(t, mockConsulEnv, "")
+	assert.NotEqual(t, mockJaegerEnv, "")
 }
