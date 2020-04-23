8df6018596f62b53c71cadb586d8b1505d507d7d
pojol <pojol@outlook.com>
2020-04-23 17:58:42 +0800

    add service test case

--- service/service_test.go
+++ service/service_test.go
@@ -1,10 +1,25 @@
 package service
 
 import (
+	"context"
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pojol/braid/caller/brpc"
+	"github.com/pojol/braid/log"
+	"google.golang.org/grpc"
 )
 
 func TestNew(t *testing.T) {
+
+	l := log.New()
+	l.Init(log.Config{
+		Path:   "test",
+		Suffex: ".log",
+		Mode:   "debug",
+	})
 
 	s := New()
 	err := s.Init(Config{
@@ -15,4 +30,28 @@ func TestNew(t *testing.T) {
 		t.Error(err)
 	}
 
+	s.Regist("test", func(ctx context.Context, in []byte) (out []byte, err error) {
+		fmt.Println("pong")
+		return nil, nil
+	})
+
+	s.Run()
+
+	conn, err := grpc.Dial("localhost"+ListenAddress, grpc.WithInsecure())
+	rres := new(brpc.RouteRes)
+
+	err = conn.Invoke(context.Background(), "/brpc.gateway/routing", &brpc.RouteReq{
+		Box:     "normal",
+		Service: "test",
+		ReqBody: nil,
+	}, rres)
+	assert.Equal(t, err, nil)
+	err = conn.Invoke(context.Background(), "/brpc.gateway/routing", &brpc.RouteReq{
+		Box:     "normal",
+		Service: "errtest",
+		ReqBody: nil,
+	}, rres)
+	assert.NotEqual(t, err, nil)
+
+	s.Close()
 }
