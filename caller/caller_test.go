d670491685361ee959aab65ae6ce4c54f4a6b357
pojol <pojol@outlook.com>
2020-04-23 16:28:22 +0800

    add test case

--- caller/caller_test.go
+++ caller/caller_test.go
@@ -1,1 +1,38 @@
 package caller
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pojol/braid/log"
+	"github.com/pojol/braid/mock"
+)
+
+func TestCaller(t *testing.T) {
+
+	mock.Init()
+	l := log.New()
+	l.Init(log.Config{
+		Path:   "test",
+		Suffex: ".log",
+		Mode:   "debug",
+	})
+
+	c := New()
+	c.Init(Config{
+		ConsulAddress: mock.ConsulAddr,
+		PoolInitNum:   8,
+		PoolCapacity:  32,
+		PoolIdle:      time.Second * 120,
+		Tracing:       false,
+	})
+
+	addr, _ := c.getBoxWithCoordinate(context.Background(), "test", "func")
+	assert.Equal(t, addr, "")
+
+	addr, _ = c.findBox(context.Background(), "test", "test", "")
+	assert.Equal(t, addr, "")
+}
--- link/link_test.go
+++ link/link_test.go
@@ -1,7 +1,47 @@
 package link
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pojol/braid/cache/redis"
+	"github.com/pojol/braid/mock"
+)
 
 func TestTarget(t *testing.T) {
+
+	mock.Init()
+
+	r := redis.New()
+	r.Init(redis.Config{
+		Address:        mock.RedisAddr,
+		ReadTimeOut:    time.Millisecond * time.Duration(5000),
+		WriteTimeOut:   time.Millisecond * time.Duration(5000),
+		ConnectTimeOut: time.Millisecond * time.Duration(2000),
+		IdleTimeout:    time.Millisecond * time.Duration(0),
+		MaxIdle:        16,
+		MaxActive:      128,
+	})
+
+	l := New()
+	l.Init(Config{})
+
+	err := l.Offline("address")
+	assert.Equal(t, err, nil)
+
+	err = l.Link("test_link", "address")
+	assert.Equal(t, err, nil)
+
+	addr, err := l.Target("test_link")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, addr, "address")
+
+	num, err := l.Num("address")
+	assert.Equal(t, num, 1)
+
+	err = l.Offline("address")
+	assert.Equal(t, err, nil)
 
 }
