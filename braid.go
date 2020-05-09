1dc42c49ce2d97b0fcb3c8dbc06f394a6ef08505
pojol <pojol@outlook.com>
2020-05-09 16:08:24 +0800

    将election容纳到service

--- braid.go
+++ braid.go
@@ -9,9 +9,9 @@ import (
 	"github.com/pojol/braid/cache/redis"
 	"github.com/pojol/braid/caller"
 	"github.com/pojol/braid/discover"
-	"github.com/pojol/braid/election"
 	"github.com/pojol/braid/log"
 	"github.com/pojol/braid/service/balancer"
+	"github.com/pojol/braid/service/election"
 	"github.com/pojol/braid/service/register"
 	"github.com/pojol/braid/tracer"
 )
