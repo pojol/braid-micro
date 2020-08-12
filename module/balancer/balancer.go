d47cfcb9e2fee999e70838c8ee8c9f5dc654f8e0
pojol <pojol@outlook.com>
2020-08-12 19:24:47 +0800

    修缮注释

--- module/balancer/balancer.go
+++ module/balancer/balancer.go
@@ -35,7 +35,7 @@ type Builder interface {
 type Balancer interface {
 	// 从服务节点列表中选取一个对应的节点，
 	// 节点列表可以订阅discover模块的消息进行填充或更改，
-	// braid 提供默认的`平滑加权轮询算法`如果有其他的需求用户可以选择实现自定义的Pick接口。
+	// braid 提供默认的`平滑加权轮询算法`如果有其他的需求，用户可以选择实现自定义的Pick接口。
 	Pick() (nod discover.Node, err error)
 }
 
