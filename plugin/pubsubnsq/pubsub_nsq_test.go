3e31998e0a8f17da5c5951b3c0bf892af55344b6
pojol <pojol@outlook.com>
2020-08-24 16:09:12 +0800

    修改测试用例

--- plugin/pubsubnsq/pubsub_nsq_test.go
+++ plugin/pubsubnsq/pubsub_nsq_test.go
@@ -32,29 +32,27 @@ func TestNsq1V1(t *testing.T) {
 		lookupAddres: []string{mock.NSQLookupdAddr},
 	})
 	pb, err := psb.Build()
-	fmt.Println(err)
 	assert.Equal(t, err, nil)
 
 	consumer := pb.Sub(topic).AddShared()
 	defer consumer.Exit()
 
+	go func() {
+		pb.Pub(topic, &pubsub.Message{Body: msgByte})
+	}()
+
 	consumer.OnArrived(func(msg *pubsub.Message) error {
-		assert.Equal(t, string(msg.Body), string(msgByte))
 		fmt.Println("test nsq 1v1 , msg arrived", string(msgByte))
 		onarrived = true
 		return nil
 	})
-
-	go func() {
-		pb.Pub(topic, &pubsub.Message{Body: msgByte})
-	}()
 
 	time.Sleep(time.Millisecond * 1000)
 	assert.Equal(t, onarrived, true)
 }
 
 func TestNsqShared(t *testing.T) {
-	topic := "test_nsq"
+	topic := "test_shared_nsq"
 	msgByte := []byte("test msg 1")
 	var onarrived uint64
 
@@ -64,7 +62,6 @@ func TestNsqShared(t *testing.T) {
 		lookupAddres: []string{mock.NSQLookupdAddr},
 	})
 	pb, err := psb.Build()
-	fmt.Println(err)
 	assert.Equal(t, err, nil)
 
 	consumer1 := pb.Sub(topic).AddShared()
@@ -73,30 +70,30 @@ func TestNsqShared(t *testing.T) {
 	consumer2 := pb.Sub(topic).AddShared()
 	defer consumer2.Exit()
 
+	go func() {
+		pb.Pub(topic, &pubsub.Message{Body: msgByte})
+	}()
+
 	consumer1.OnArrived(func(msg *pubsub.Message) error {
-		fmt.Println("consumer 1 msg arrived", string(msgByte))
+		fmt.Println("TestNsqShared consumer 1 msg arrived", string(msgByte))
 
 		atomic.AddUint64(&onarrived, 1)
 		return nil
 	})
 
 	consumer2.OnArrived(func(msg *pubsub.Message) error {
-		fmt.Println("consumer 2 msg arrived", string(msgByte))
+		fmt.Println("TestNsqShared consumer 2 msg arrived", string(msgByte))
 
 		atomic.AddUint64(&onarrived, 1)
 		return nil
 	})
-
-	go func() {
-		pb.Pub(topic, &pubsub.Message{Body: msgByte})
-	}()
 
 	time.Sleep(time.Millisecond * 1000)
 	assert.Equal(t, onarrived, uint64(2))
 }
 
 func TestNsqCompetition(t *testing.T) {
-	topic := "test_nsq"
+	topic := "test_competion_nsq"
 	msgByte := []byte("test msg 1")
 	var onarrived uint64
 
@@ -107,8 +104,11 @@ func TestNsqCompetition(t *testing.T) {
 		lookupAddres: []string{mock.NSQLookupdAddr},
 	})
 	pb, err := psb.Build()
-	fmt.Println(err)
 	assert.Equal(t, err, nil)
+
+	go func() {
+		pb.Pub(topic, &pubsub.Message{Body: msgByte})
+	}()
 
 	consumer1 := pb.Sub(topic).AddCompetition()
 	defer consumer1.Exit()
@@ -117,22 +117,18 @@ func TestNsqCompetition(t *testing.T) {
 	defer consumer2.Exit()
 
 	consumer1.OnArrived(func(msg *pubsub.Message) error {
-		fmt.Println("consumer 1 msg arrived", string(msgByte))
+		fmt.Println("TestNsqCompetition consumer 1 msg arrived", string(msgByte))
 
 		atomic.AddUint64(&onarrived, 1)
 		return nil
 	})
 
 	consumer2.OnArrived(func(msg *pubsub.Message) error {
-		fmt.Println("consumer 2 msg arrived", string(msgByte))
+		fmt.Println("TestNsqCompetition consumer 2 msg arrived", string(msgByte))
 
 		atomic.AddUint64(&onarrived, 1)
 		return nil
 	})
-
-	go func() {
-		pb.Pub(topic, &pubsub.Message{Body: msgByte})
-	}()
 
 	time.Sleep(time.Millisecond * 1000)
 	assert.Equal(t, onarrived, uint64(1))
