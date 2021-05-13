de540b99b28733dcda261c029691b57fae11e862
pojol <pojol@outlook.com>
2021-05-13 22:47:33 +0800

    # fix - 修复测试用例编写错误

--- modules/mailboxnsq/mailbox_proc_test.go
+++ modules/mailboxnsq/mailbox_proc_test.go
@@ -64,15 +64,15 @@ func TestProcExit(t *testing.T) {
 
 	topic.Pub(&mailbox.Message{Body: []byte("msg")})
 
-	err := topic.Pub(&mailbox.Message{Body: []byte("msg")})
-	assert.NotEqual(t, err, nil)
-
 	select {
 	case <-time.After(time.Second * 2):
-		err = mb.RemoveTopic("TestProcExit")
+		assert.Equal(t, atomic.LoadUint64(&tick), uint64(1))
+
+		err := mb.RemoveTopic("TestProcExit")
 		assert.Equal(t, err, nil)
 
-		assert.Equal(t, atomic.LoadUint64(&tick), uint64(1))
+		err = topic.Pub(&mailbox.Message{Body: []byte("msg")})
+		assert.NotEqual(t, err, nil)
 	}
 }
 
