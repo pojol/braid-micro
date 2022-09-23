51acfe0fd93dccdaa59a353c1856f6759f5088dc
pojol <pojol@outlook.com>
2022-09-23 19:58:57 +0800

    fix : 缺少传递 pubsub

--- module/internal/discoverconsul/discover_test.go
+++ module/internal/discoverconsul/discover_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/pojol/braid-go/depend/consul"
 	"github.com/pojol/braid-go/mock"
 	"github.com/pojol/braid-go/module/discover"
+	"github.com/pojol/braid-go/module/internal/pubsubnsq"
+	"github.com/pojol/braid-go/module/pubsub"
 )
 
 func TestMain(m *testing.M) {
@@ -18,10 +20,12 @@ func TestMain(m *testing.M) {
 
 func TestDiscover(t *testing.T) {
 
+	log := blog.BuildWithOption()
+
 	d := BuildWithOption(
 		"base_dev",
-		blog.BuildWithOption(),
-		nil,
+		log,
+		pubsubnsq.BuildWithOption("", log, pubsub.WithNsqdAddr([]string{mock.NsqdAddr}, []string{mock.NsqdHttpAddr})),
 		consul.BuildWithOption(consul.WithAddress([]string{mock.ConsulAddr})),
 		discover.WithSyncServiceInterval(time.Millisecond*100),
 		discover.WithSyncServiceWeightInterval(time.Millisecond*100),
